bbad9a17c91b5ab9f445c377a27a4a56b1fc0c6f
jbrizuela2 <jbrizuela@ucol.mx>
2018-12-06 12:36:34 -0600

    File uploads are persisted across form re-displays

--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -39,17 +39,30 @@ scenario "with an invalid description" do
   expect(page).to have_content "Description is too short"
 end
 
-  scenario "with an attachment" do
-    fill_in "Name", with: "Add documentation for blink tag"
-    fill_in "Description", with: "The blink tag has a speed attribute"
-    attach_file "File", "spec/fixtures/speed.txt"
-    click_button "Create Ticket"
+scenario "with an attachment" do
+  fill_in "Name", with: "Add documentation for blink tag"
+  fill_in "Description", with: "The blink tag has a speed attribute"
+  attach_file "File", "spec/fixtures/speed.txt"
+  click_button "Create Ticket"
 
-    expect(page).to have_content "Ticket has been created"
+  expect(page).to have_content "Ticket has been created"
 
-    within("#ticket .attachment") do
-      expect(page).to have_content "speed.txt"
-    end
+  within("#ticket .attachment") do
+    expect(page).to have_content "speed.txt"
   end
+end
+
+scenario "persisting file uploads across form displays" do
+  attach_file "File", "spec/fixtures/speed.txt"
+  click_button "Create Ticket"
+
+  fill_in "Name", with: "Add documentation for blink tag"
+  fill_in "Description", with: "The blink tag has a speed attribute"
+  click_button "Create Ticket"
+
+  within("#ticket .attachment") do
+    expect(page).to have_content "speed.txt"
+  end
+end
 
 end
