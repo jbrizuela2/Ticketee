cb6e1d638d57dfd3dadc9551f593c788ada93478
jbrizuela2 <jbrizuela@ucol.mx>
2018-12-06 13:03:15 -0600

    Attached files persist after re-displays

--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -11,58 +11,70 @@ RSpec.feature "Admins can create new tickets" do
     click_link "New Ticket"
   end
 
-  scenario "with valid attributes" do
-    fill_in "Name", with: "Non-standards compliance"
-    fill_in "Description", with: "My pages are ugly!"
+    scenario "with valid attributes" do
+      fill_in "Name", with: "Non-standards compliance"
+      fill_in "Description", with: "My pages are ugly!"
+      click_button "Create Ticket"
+
+      expect(page).to have_content "Ticket has been created."
+      within("#ticket") do
+        expect(page).to have_content "Author: #{user.email}"
+      end
+    end
+
+    scenario "when providing invalid attributes" do
+      click_button "Create Ticket"
+
+      expect(page).to have_content "Ticket has not been created."
+      expect(page).to have_content "Name can't be blank"
+      expect(page).to have_content "Description can't be blank"
+    end
+
+  scenario "with an invalid description" do
+    fill_in "Name", with: "Non-standars compliance"
+    fill_in "Description", with: "It sucks"
     click_button "Create Ticket"
 
-    expect(page).to have_content "Ticket has been created."
-    within("#ticket") do
-      expect(page).to have_content "Author: #{user.email}"
+    expect(page).to have_content "Ticket has not been created"
+    expect(page).to have_content "Description is too short"
+  end
+
+  scenario "with an attachment" do
+    fill_in "Name", with: "Add documentation for blink tag"
+    fill_in "Description", with: "The blink tag has a speed attribute"
+    attach_file "File", "spec/fixtures/speed.txt"
+    click_button "Create Ticket"
+
+    expect(page).to have_content "Ticket has been created"
+
+    within("#ticket .attachment") do
+      expect(page).to have_content "speed.txt"
     end
   end
 
-  scenario "when providing invalid attributes" do
+  scenario "persisting file uploads across form displays" do
+    attach_file "File", "spec/fixtures/speed.txt"
     click_button "Create Ticket"
 
-    expect(page).to have_content "Ticket has not been created."
-    expect(page).to have_content "Name can't be blank"
-    expect(page).to have_content "Description can't be blank"
+    fill_in "Name", with: "Add documentation for blink tag"
+    fill_in "Description", with: "The blink tag has a speed attribute"
+    click_button "Create Ticket"
+
+    within("#ticket .attachment") do
+      expect(page).to have_content "speed.txt"
+    end
   end
 
-scenario "with an invalid description" do
-  fill_in "Name", with: "Non-standars compliance"
-  fill_in "Description", with: "It sucks"
-  click_button "Create Ticket"
+  scenario "with an attachment" do
+    fill_in "Name", with: "Add documentation for blink tag"
+    fill_in "Description", with: "The blink tag has a speed attribute"
+    attach_file "File", "spec/fixtures/speed.txt"
+    click_button "Create Ticket"
 
-  expect(page).to have_content "Ticket has not been created"
-  expect(page).to have_content "Description is too short"
-end
+    expect(page).to have_content "Ticket has been created"
 
-scenario "with an attachment" do
-  fill_in "Name", with: "Add documentation for blink tag"
-  fill_in "Description", with: "The blink tag has a speed attribute"
-  attach_file "File", "spec/fixtures/speed.txt"
-  click_button "Create Ticket"
-
-  expect(page).to have_content "Ticket has been created"
-
-  within("#ticket .attachment") do
-    expect(page).to have_content "speed.txt"
+    within("#ticket .attachment") do
+      expect(page).to have_content "speed.txt"
+    end
   end
-end
-
-scenario "persisting file uploads across form displays" do
-  attach_file "File", "spec/fixtures/speed.txt"
-  click_button "Create Ticket"
-
-  fill_in "Name", with: "Add documentation for blink tag"
-  fill_in "Description", with: "The blink tag has a speed attribute"
-  click_button "Create Ticket"
-
-  within("#ticket .attachment") do
-    expect(page).to have_content "speed.txt"
-  end
-end
-
 end
